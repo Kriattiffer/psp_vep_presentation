0573e8f1b727b8756007be6ce0bc8a49daeace63
Kriattiffer <kriattiffer@gmail.com>
2016-08-15 21:09:47 +0300

    w1

--- present.py
+++ present.py
@@ -1,3 +1,77 @@
-from psychopy import visual
+from psychopy import visual, core, event, monitors
 
-pass
+import os, sys, time
+
+mymon = monitors.Monitor('Eizo', distance=50, width = 52.5)
+# mymon = monitors.Monitor('zenbook', distance=50, width = 29.5)
+
+mymon.setSizePix([1920, 1080])	
+
+
+stimrad = 2
+stimcolor = 'red'
+base_mseq = [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0] # steady-state
+# base_mseq = [0,0,0,0,0,0,1,1,1,1,1,0,1,1,1,1,0,0,1,1,1,0,1,0,1,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,0,1,1,0,1,0,0,1,0,0,0,1,0,0,1,1,0,0,1,0,1,0,1,0] # cvep
+
+win = visual.Window(fullscr = True,
+					rgb = '#868686',
+					size=(1920, 1080),
+					monitor = mymon)
+
+cell1 = visual.Circle(win,
+						radius=stimrad,
+						lineColor = None, 
+						fillColor = stimcolor,
+						units = 'deg',
+						autoDraw=True
+						)
+cell2 = visual.Circle(win,
+						radius=stimrad,
+						lineColor = None, 
+						fillColor = stimcolor,
+						units = 'deg',
+						autoDraw=True
+						)
+cell3 = visual.Circle(win,
+						radius=stimrad,
+						lineColor = None, 
+						fillColor = stimcolor,
+						units = 'deg',
+						autoDraw=True
+						)
+cell4 = visual.Circle(win,
+						radius=stimrad,
+						lineColor = None, 
+						fillColor = stimcolor,
+						units = 'deg',
+						autoDraw=True
+						)
+
+fixation = visual.GratingStim(win, size=10, units = 'pix', pos=[0,0], autoDraw=True)
+
+cell1.pos = [0, 12]
+cell2.pos = [12, 0]
+cell3.pos = [0, -12]
+cell4.pos = [-12, 0]
+
+
+
+def flipper(n, frame):
+	# if 27<=n<=30:
+	# 	cell.fillColor = 'red'
+	# 	win.flip()
+	if frame ==1:
+		cell1.fillColor = 'white'
+		win.flip()
+		return
+	elif frame == 0:
+		cell1.fillColor = 'black'
+		win.flip()
+		return
+
+while  1:
+
+	if 'escape' in event.getKeys():
+		sys.exit()
+	for n, frame in enumerate(base_mseq):
+		flipper(n, frame)	
