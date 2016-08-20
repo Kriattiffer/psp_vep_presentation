089f59dd2b788bdc81379a69b722d84dd7df73a0
Kriattiffer <kriattiffer@gmail.com>
2016-08-20 21:17:23 +0300

    Added sync via LSL; added EEG recording

--- present.py
+++ present.py
@@ -1,4 +1,5 @@
 from psychopy import visual, core, event, monitors
+from pylsl import StreamInfo, StreamOutlet
 import os, sys, time, socket
 import numpy
 
@@ -15,9 +16,10 @@ base_mseq = [0,0,0,0,0,0,1,1,1,1,1,0,1,1,1,1,0,0,1,1,1,0,1,0,1,1,0,0,2,2,1,0,1,1
 base_mseq = numpy.array(base_mseq, dtype = int)
 
 
+
 class ENVIRONMENT():
 	""" class for visual stimulation during the experiment """
-
+	
 	def build_gui(self, monitor = mymon, size=(1920, 1080),
 	 			  rgb = '#868686', stimrad = 2, stimcolor = 'red', fix_size = 1):
 		''' function for creating visual enviroment. Input: various parameters of stimuli, all optional'''
@@ -80,31 +82,24 @@ class ENVIRONMENT():
 			CELL.fillColor = 'black'
 			return
 
-	def create_steady_state_sequences(self, freqs, refresh_rate = 120, limit_pulse_width = None, phase_rotate = None):
-		'''Function receives list of frequencies and screen refresh_rate, returns list of sequences, corresponding to this frequencies. 
-		   Frequences should be disisors of refresh_rate/2
-		   TODO:
-		   If limit_pulse_width (int) is specified, number of consequitive 'ones' is limited by this number 
-		   If duty_cycles (list) is specified, sequences will have corresponding duty cycles
- 		   If phase_rotate (list) is specified, sequences will be shifted by corresponding fraction of its period  '''
-		ss_seqs = []
-		for freq in freqs:
-			ss_seq = [a  for a in [1,0]*freq for b in range(refresh_rate/2/freq)]
-			ss_seqs.append(ss_seq)
-		return ss_seqs
-
 	def run_exp(self, base_mseq):
-		tt = time.time()
-
-
+		'''Core function of the experiment. Defines GUI behavior and marker sending'''
 		# create sequences for every cell; should be the same length!
 		# seq1, seq2, seq3, seq4 = base_mseq, numpy.roll(base_mseq, 8), numpy.roll(base_mseq, 16), numpy.roll(base_mseq, 24) # CVEP
-		steady_state_seqs = self.create_steady_state_sequences([6, 10, 12, 15])
+		steady_state_seqs = create_steady_state_sequences([6, 10, 12, 15])
 		seq1, seq2, seq3, seq4 = steady_state_seqs[0], steady_state_seqs[1], steady_state_seqs[2], steady_state_seqs[3]
 		pattern = [(self.cell1, seq1), (self.cell2, seq2), (self.cell3, seq3), (self.cell4, seq4)]
+		
+		LSL = create_lsl_outlet() # create outlet for sync with NIC
+		core.wait(1)
+		LSL.push_sample([11]) # indicate the begining of trial
+
+		tt = time.time()
 		while  1:
 			if 'escape' in event.getKeys():
 				sys.exit()
+			
+			LSL.push_sample([111])  # sync with EEG
 
 			for bit_number in range(len(seq4)): # cycle through sequences 
 				for pair in pattern: # decide what to do with every circle at the next refresh
@@ -112,9 +107,28 @@ class ENVIRONMENT():
 				
 				self.win.flip() # refresh screen
 			
-			print (1 + (tt - time.time()))*1000
+			print (1 + (tt - time.time()))*1000 # difference between desired and real time
 			tt = time.time()
 
+
+def create_steady_state_sequences(freqs, refresh_rate = 120, limit_pulse_width = None, phase_rotate = None):
+	'''Function receives list of frequencies and screen refresh_rate, returns list of sequences, corresponding to this frequencies. 
+	   Frequences should be disisors of refresh_rate/2
+	   TODO:
+	   If limit_pulse_width (int) is specified, number of consequitive 'ones' is limited by this number 
+	   If duty_cycles (list) is specified, sequences will have corresponding duty cycles
+	   If phase_rotate (list) is specified, sequences will be shifted by corresponding fraction of its period  '''
+	ss_seqs = []
+	for freq in freqs:
+		ss_seq = [a  for a in [1,0]*freq for b in range(refresh_rate/2/freq)]
+		ss_seqs.append(ss_seq)
+	return ss_seqs
+
+def create_lsl_outlet(name = 'CycleStart', DeviceMac = '00:07:80:64:EB:46'):
+	''' Create outlet for Enobio. Requires name of the stream (same as in NIC) and MAC adress of the device. Returns outlet object. Use by Outlet.push_sample([MARKER_INT])'''
+	info = StreamInfo(name,'Markers',1,0,'int32', DeviceMac)
+	outlet =StreamOutlet(info)
+	return outlet
 
 if __name__ == '__main__':
 	
--- record.py
+++ record.py
@@ -1,2 +1,66 @@
+from pylsl import StreamInlet, resolve_stream
 import numpy as np
 import socket
+
+def create_streams(stream_name_eeg = 'NIC', stream_name_markers = 'CycleStart'):
+	''' Opens two LSL streams: one for EEG, another for markers'''
+	
+	print ("Connecting to NIC stream...")
+	streams_eeg = resolve_stream('type', 'EEG')
+	inlet_eeg = StreamInlet(streams_eeg[0])   
+	try:
+		inlet_eeg
+		print '...done \n'
+	except NameError:
+		print ("Error: NIC stream not available\n")
+
+
+	print ("Connecting to Psychopy stream...")
+	sterams_markers = resolve_stream('name', stream_name_markers)	
+	inlet_markers = StreamInlet(sterams_markers[0])   
+	try:
+		inlet_markers
+		print '...done \n'
+
+	except NameError:
+		print ("Error: Psychopy stream not available\n")
+
+	return inlet_eeg, inlet_markers
+
+def create_array(top_exp_length = 60):
+	"Creates very long array of Nans, which will be filled by EEG. length is determined by maximum length of the experiment in minutes"
+	record_length = 500*60*top_exp_length*1.2
+	array_size = (9, record_length)
+	print 'Creating array with dimensions %s...' %str(array_size) 
+	a = np.ones((record_length, 9), dtype = 'float')
+	a[:] = np.NAN
+	print '... done'
+	return a
+
+def fill_array(eeg_array, line_counter, data_chunk, timestamp_chunk, datatype = 'EEG'):
+	length = len(timestamp_chunk)
+
+	eeg_array[line_counter:line_counter+length, 0] = timestamp_chunk
+	eeg_array[line_counter:line_counter+length,1:] = data_chunk
+	# print eeg_array
+
+
+ie, im =  create_streams()
+EEG_ARRAY = create_array()
+
+line_counter = 0
+while line_counter<800:
+    print line_counter
+    marker, timestamp1 = im.pull_chunk()
+    if timestamp1:
+    	pass
+    	# line_counter += len(timestamp2)
+        # print('MARKER', timestamp1, marker)
+	# EEG = ie.pull_chunk()    
+    EEG, timestamp2 = ie.pull_chunk()
+    if timestamp2:
+    	fill_array(EEG_ARRAY, line_counter, EEG, timestamp2, datatype = 'EEG')
+    	line_counter += len(timestamp2)
+
+        # print('eeg', timestamp2, EEG)
+np.savetxt('data.txt', EEG_ARRAY, fmt= '%.4f')
