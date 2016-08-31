bbd48505f9a8ae3ed2f403e748ed795b5a0c5f3b
Kriattiffer <kriattiffer@gmail.com>
2016-08-31 20:36:21 +0300

    started work on classifier

--- classify.py
+++ classify.py
@@ -2,46 +2,15 @@ import numpy as np
 import socket
 from matplotlib import pyplot  as plt 
 from scipy import signal
-pass
 
-max_fft_freq = 100
+class Classifier():
+	"""docstring for Classifier"""
+	def __init__(self, mapnames):
+		pass
+		# self.eeg = np.memmap(mmapnames['eeg'], dtype='float', mode='r', shape=(array_shape))
+		# self.markers = np.memmap(mmapnames['markers'], dtype='float', mode='r', shape=(array_shape))
 
-data = np.genfromtxt('_data.txt')
-markers = np.genfromtxt('_markers.txt')
 
-def butter_filt(data, cutoff_array, fs = 500, order=4):
-    nyq = 0.5 * fs
-    normal_cutoff = [a /nyq for a in cutoff_array]
-    b, a = signal.butter(order, normal_cutoff, btype = 'bandpass', analog=False)
-    data = signal.filtfilt(b, a, data, axis = 0)
-    return data
-
-def PSDA(data_chunk):
-	pass
-
-offsets = []
-for a in markers[:-1]:
-	offsets.append(np.where(data[:,0]>a[0])[0][0])
-letters =  np.reshape(offsets, (12,2))
-print letters
-
-data =  butter_filt(data, [4, 80])
-
-for letter in letters:
-	data_chunk = data[letter[0]: letter[-1],:]
-	print  np.shape(data_chunk)
-	FFT = np.zeros((max_fft_freq,9))
-	# for i in range(2,5):
-	# 	fft =  np.abs(np.fft.fft(data_chunk[500*i: 500*(i+1),:], axis = 0))[:max_fft_freq,:]
-	# 	FFT +=fft
-	# fft  = FFT/len(range(1,5))
-	
-	fft =  np.abs(np.fft.fft(data_chunk, axis = 0))[:max_fft_freq,:]
-	fft = fft[:,1]
-	
-	x = np.arange(0, max_fft_freq/2, 0.5,)	
-	plt.plot(x, fft)
-	plt.title(np.argmax(fft)/2)
-	plt.get_current_fig_manager().window.wm_geometry("-1920+0") # move FFT window tio second screen. Frame redraw in pesent.py starts to suck ==> possible problem with video card
-
-	plt.show()
+if __name__ == '__main__':
+	mapnames = {'eeg':'eegdata.mmap', 'markers':'markers.mmap'}
+	Classifier(mapnames)
--- launcher.py
+++ launcher.py
@@ -1,19 +1,21 @@
 import multiprocessing, sys
+from tempfile import mkdtemp
+
 import numpy as np
 import present
 import record
-# import classify
+import classify
 
 mapnames = {'eeg':'eegdata.mmap', 'markers':'markers.mmap'}
-
+print mkdtemp()
 def view():
 	'''create stimulation window'''
 	ENV = present.ENVIRONMENT()
 	ENV.Fullscreen = True
 	ENV.refresh_rate = 120
 	ENV.build_gui(monitor = present.mymon, rgb = ENV.rgb)
-	ENV.run_exp()
-	# ENV.run_P300_exp()
+	# ENV.run_exp()
+	ENV.run_P300_exp()
 	sys.stdout = open(str(os.getpid()) + ".out", "w")
 
 def rec():
@@ -22,11 +24,19 @@ def rec():
 	Stream.plot_and_record()
 	sys.stdout = open(str(os.getpid()) + ".out", "w")
 
+def class_():
+	Classifier = classify.Classifier(mapnames = mapnames)
+	sys.stdout = open(str(os.getpid()) + ".out", "w")
+
+
 if __name__ == '__main__':
 	print 'startig GUI...'
 	p1 = multiprocessing.Process(target=view)
 	print 'startig backend...'
 	p2 = multiprocessing.Process(target=rec)
+	print 'startig classifier...'
+	p2 = multiprocessing.Process(target=class_)
+
 	p2.start()
 	p1.start()
 
