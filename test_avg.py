f9ccfafdb44f15d32441df1a4cf79ac0f09c4e8d
Kriattiffer <kriattiffer@gmail.com>
2016-10-24 15:05:51 +0300

    timing

--- test_avg.py
+++ test_avg.py
@@ -31,7 +31,7 @@ def get_maximums_from_eeg(eeg, window_length = 140):
 			try:
 				delta = (mx-maxlist[-1])*1000 
 				if abs(delta)<window_length:
-					pass
+					pass	
 					# print mx
 					# print maxlist[-1]
 					# print a
@@ -41,18 +41,26 @@ def get_maximums_from_eeg(eeg, window_length = 140):
 					# plt.plot(amold, 'o')
 
 					# plt.show()
+				else:
+					pass
+					maxlist.append(mx)
+
 			except:
 				pass
-			maxlist.append(mx)
+			
+			if len(maxlist) == 0:
+			# if 1:
+				maxlist.append(mx)
 
 	maxinds =  np.array(maxlist)
-	return maxinds
-	# minddelta = (maxinds[1:] - maxinds[:-1])*1000
-	# print minddelta
+	minddelta = (maxinds[1:] - maxinds[:-1])*1000
 	# plt.hist(minddelta, bins = 200)
 	# plt.show()
-	# plt.plot(minddelta,"o")
-	# plt.show()
+	print 'delta t maximums'
+	plt.plot(minddelta,"o")
+	plt.show()
+	return maxinds
+
 
 def find_nearest(array,value):
     idx = (np.abs(array-value)).argmin()
@@ -61,12 +69,11 @@ def find_nearest(array,value):
 def from_LSL(mend = 999, mstart = 777):
 	markers = np.genfromtxt('./_markers240_200.txt')
 
+
+
 	eeg = np.genfromtxt('./_data240_200.txt')
 	# eeg[:,1:] = butter_filt(eeg[:,1:], (1,40))
 	max_inds  = get_maximums_from_eeg(eeg)
-
-
-	aims = [int(a)-1 for a in np.genfromtxt('aims_play.txt')]
 
 
 	mmm = np.logical_and(markers[:,1]!=mstart, markers [:,-1] !=mend)
@@ -84,16 +91,25 @@ def from_LSL(mend = 999, mstart = 777):
 		nn = find_nearest(max_inds, a)
 		NN.append(nn)
 	NN = np.array(NN)
+	
 	deltas = NN - markers[:,0]
-	plt.plot(deltas - deltas[0], 'o')
-	plt.show()
 
-	# deltaof = np.round(deltaof, 3)
+	deltaof = np.round(deltaof, 3)
 	# plt.hist(deltaof, bins=15)
 	# plt.show()
 	# plt.clf()
-	# plt.plot(deltaof, 'o')
-	# plt.show()
+	print 'delta t markers'
+	plt.plot(deltaof*1000, 'o')
+	plt.show()
+
+	print 'delta t markers from 0'
+	deltamarkers = markers[:,0] - markers[0,0]
+	plt.plot(deltamarkers, 'o')
+	plt.show()
+
+	print 'delta t between markers and maximums'
+	plt.plot(deltas - deltas[0], 'o')
+	plt.show()
 	# plt.clf()
 
 	sleeg = slice_eeg(offsts, eeg)
@@ -105,12 +121,23 @@ def from_plain_eeg():
 	eeg = np.genfromtxt('./_data240_200.txt')
 	offs = np.array( range(0, np.shape(eeg)[0], 100))/1000 + eeg[0,0]
 	sleeg = slice_eeg(offs, eeg)
+
+	max_inds  = get_maximums_from_eeg(eeg)
+	NN = []
+	for a in offs:
+		nn = find_nearest(max_inds, a)
+		NN.append(nn)
+	NN = np.array(NN)
+	deltas = NN - offs
+	plt.plot(deltas - deltas[0], 'o')
+	plt.show()
+
 	print np.shape(sleeg)
 	return sleeg
 
 
-# slices = from_LSL()
-slices = from_plain_eeg()
+slices = from_LSL()
+# slices = from_plain_eeg()
 
 
 print np.shape(slices)
